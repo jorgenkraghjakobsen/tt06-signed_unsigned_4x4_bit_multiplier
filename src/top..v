ba6d3b2b85193678f554e2e93556bda598bbdbe2
Jorgen Kragh Jakobsen <jkj@myrun.dk>
2024-04-14 22:48:14 +0200

    added convtered multiplier

--- src/top..v
+++ src/top..v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_example (
+module tt_um_signed_unsigned_4x4_bit_multiplier(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,8 +17,19 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
+  
+  // Insert multiplier logic here 
+  signed_unsigned_4x4_bit_multiplier mult( 
+    .multiplicand(ui_in[7:4]),
+    .multiplier(ui_in[3:0]),
+    .signed_mode(uio_in[0]),
+    .product(uo_out[7:0]));
+
+    
+
+
 
 endmodule
